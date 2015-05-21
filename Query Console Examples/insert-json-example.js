506647daa071a1e1d17b3de8b76c27e3f3c2975b
stephen-gehring-ml <stephen.gehring@marklogic.com>
2015-05-20 22:56:17 -0700

    adding q console examples

---
+++ Query Console Examples/insert-json-example.js
@@ -0,0 +1,28 @@
+//https://docs.marklogic.com/xdmp.documentInsert?q=documentInsert&v=8.0&api=true
+
+
+/*
+
+xdmp.documentInsert(
+   $uri as String,
+   $root as Node,
+   [$permissions as Object[]],
+   [$collections as String[]],
+   [$quality as xs.int?],
+   [$forest-ids as String[]]
+) as null
+
+*/
+
+// BASIC ML INSERT COMMAND 
+declareUpdate();
+var obj = {key1:"value1", key2:"value2"}; xdmp.documentInsert("/simple.json", obj);
+xdmp.documentInsert("/simple.json", obj);
+
+
+// BASIC ML INSERT COMMAND W/ PERMISSIONS
+declareUpdate();
+var perms = [xdmp.permission("qconsole-user", "read"), xdmp.permission("qconsole-user", "update")];
+xdmp.documentInsert("/fox.json", fox, perms, col);
+
+
---
+++ Query Console Examples/search-example.js
@@ -0,0 +1,11 @@
+
+
+// BASIC ML SEARCH COMMAND
+cts.search("keyword");
+
+
+// BASIC ML SEARCH FOR LOOP
+for (var result of cts.search("drove")) {
+  count++;
+  results.push(result); 
+};
